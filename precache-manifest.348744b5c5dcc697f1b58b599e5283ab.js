cbacdece73067bdc91b7a0ce3fcdd2dff7437e20
Hillary Peters <hillarypeters233@gmail.com>
2019-09-22 00:05:29 +0100

    My Robofriends App

---
+++ precache-manifest.348744b5c5dcc697f1b58b599e5283ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd3abaf956dd74cc48c9e8a2f837459",
+    "url": "/index.html"
+  },
+  {
+    "revision": "463ed1e2df2d257000ed",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ad5c86193af4458c7b14",
+    "url": "/static/css/main.c2707fea.chunk.css"
+  },
+  {
+    "revision": "463ed1e2df2d257000ed",
+    "url": "/static/js/2.b2a8e7af.chunk.js"
+  },
+  {
+    "revision": "ad5c86193af4458c7b14",
+    "url": "/static/js/main.4e329970.chunk.js"
+  },
+  {
+    "revision": "2fea5e571640de4687f7",
+    "url": "/static/js/runtime-main.83bf741c.js"
+  }
+]);
